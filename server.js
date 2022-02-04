7111211f9e354aa88931a5078f6aedabfd6a3833
Igor S <ermygut@gmail.com>
2022-02-04 13:20:56 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, 'build');
+
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(process.env.PORT || 5000, () => {
+	console.log('Server is up!');
+});
